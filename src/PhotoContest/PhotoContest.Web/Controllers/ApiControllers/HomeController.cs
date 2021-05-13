3a9db0addaf62c6e76d400338d353cc7f43cd631
IvanDichev <i.dichev0@gmail.com>
2021-05-13 15:00:29 +0300

    Initial structure

---
+++ src/PhotoContest/PhotoContest.Web/Controllers/ApiControllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoContest.Web.Controllers.ApiControllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("In API Controller!");
+        }
+    }
+}
---
+++ src/PhotoContest/PhotoContest.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhotoContest.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
