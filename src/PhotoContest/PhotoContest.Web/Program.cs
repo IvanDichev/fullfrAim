44e2cb6ce2388b0ab900fd0340e143b28406f785
IvanDichev <i.dichev0@gmail.com>
2021-05-13 17:40:30 +0300

    Set initial logger settings

--- src/PhotoContest/PhotoContest.Web/Program.cs
+++ src/PhotoContest/PhotoContest.Web/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace PhotoContest.Web
 {
@@ -13,11 +9,32 @@ namespace PhotoContest.Web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(path: "Logs\\LoggedInformation.txt",
+                    rollingInterval: RollingInterval.Day,
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
+                    fileSizeLimitBytes: 1024 * 1024, // 1Mb
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex.Message);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
