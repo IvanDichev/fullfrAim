2506f41930c143dd7ffc8b2445e332f9371ff2d2
Valentin Shikov <valentin.shikov.a26@learn.telerikacademy.com>
2021-05-14 12:31:17 +0300

    Remove unwated comment

--- src/PhotoContest/PhotoContest.Data/Base/BaseEntity.cs
+++ src/PhotoContest/PhotoContest.Data/Base/BaseEntity.cs
@@ -8,7 +8,6 @@ namespace PhotoContest.Data.Base
         [Key]
         public T Id { get; set; }
 
-        //Vanka: Triabva li da e required :D
         [Required]
         public DateTime CreatedOn { get; set; }
         public DateTime? ModifiedOn { get; set; }
