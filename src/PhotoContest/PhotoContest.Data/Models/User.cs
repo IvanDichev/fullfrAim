a83b18d1a4aa9d844e41683426babed1b63fc3f5
boryana.gm <boryana.gm@gmail.com>
2021-05-14 14:09:51 +0300

    Revert "Deleted files from working directory"
    This reverts commit 8260c170e93875239b19ebb4d8ccc32e9e775c4b.

---
+++ src/PhotoContest/PhotoContest.Data/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhotoContest.Data.Models
+{
+    public class User : IdentityUser<int>
+    {
+        [Required]
+        [StringLength(maximumLength: 20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 20)]
+        public string LastName { get; set; }
+
+        public uint Points { get; set; }
+
+        public ICollection<UserContest> UserContests { get; set; }
+
+        public ICollection<Photo> Photos { get; set; }
+
+        public ICollection<PhotoReview> PhotoReviews { get; set; }
+    }
+}
