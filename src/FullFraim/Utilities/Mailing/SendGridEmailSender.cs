143e8161ad6d4befc4b817c0306f6b3c858df0a9
boryana.gm <boryana.gm@gmail.com>
2021-05-16 14:59:04 +0300

    add SendGridEmailSender class

---
+++ src/FullFraim/Utilities/Mailing/SendGridEmailSender.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Mailing
+{
+    public class SendGridEmailSender : IEmailSender
+    {
+        //private readonly SendGridClient client;
+        //public SendGridEmailSender(string apiKey)
+        //{
+        //    this.client = new SendGridClient(apiKey);
+        //}
+        public Task SendEmailAsync(string from, string fromName, string to, string subject, string htmlContent, IEnumerable<EmailAttachment> attachments = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
