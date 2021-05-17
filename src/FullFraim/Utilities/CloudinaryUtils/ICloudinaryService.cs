a05324c8178c3281e3c7a683a62511bc07c17cc5
IvanDichev <i.dichev0@gmail.com>
2021-05-17 18:32:48 +0300

    Fix task/async cloudinary upload

--- src/FullFraim/Utilities/CloudinaryUtils/ICloudinaryService.cs
+++ src/FullFraim/Utilities/CloudinaryUtils/ICloudinaryService.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System.Threading.Tasks;
 
 namespace Utilities.CloudinaryUtils
 {
     public interface ICloudinaryService
     {
-        Task<string> UploadImage(IFormFile file, string extention = ".png");
+        string UploadImage(IFormFile file, string extention = ".png");
     }
 }
