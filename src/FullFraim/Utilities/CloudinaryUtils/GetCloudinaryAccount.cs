c49235f6427ced7147aa4526ac9f510dff72eb89
IvanDichev <i.dichev0@gmail.com>
2021-05-17 11:44:08 +0300

    Add cloudinary upload

---
+++ src/FullFraim/Utilities/CloudinaryUtils/GetCloudinaryAccount.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+
+namespace Utilities.CloudinaryUtils
+{
+    public class GetCloudinary
+    {
+        private readonly IConfiguration config;
+        private readonly Account account;
+        private readonly Cloudinary cloudinary;
+
+        public GetCloudinary(IConfiguration config)
+        {
+            this.config = config;
+
+            this.account = new Account(
+                this.config["Cloudinary:CloudName"],
+                this.config["Cloudinary:ApiKey"],
+                this.config["Cloudinary:ApiSecret"]);
+
+            this.cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<string> UploadFileAs(IFormFile file, string extention = ".png")
+        {
+            string filePath = Guid.NewGuid().ToString();
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(filePath + extention, file.OpenReadStream()),
+                Overwrite = true,
+            };
+
+            var uploadResult = await this.cloudinary.UploadAsync(uploadParams);
+
+            return uploadResult.SecureUrl.AbsoluteUri;
+        }
+    }
+}
