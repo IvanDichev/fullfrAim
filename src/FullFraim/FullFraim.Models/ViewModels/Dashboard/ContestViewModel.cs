bafe95c7d8fefffe1ead700ee48cf16f4899ef47
boryana.gm <boryana.gm@gmail.com>
2021-05-19 13:10:49 +0300

    Add ContestViewModel

--- src/FullFraim/FullFraim.Models/ViewModels/Dashboard/ContestViewModel.cs
+++ src/FullFraim/FullFraim.Models/ViewModels/Dashboard/ContestViewModel.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FullFraim.Models.ViewModels.Dashboard
 {
     public class ContestViewModel
     {
+        //public int TestProperty { get; set; }
+        //public List<string> TestListProperty { get; set; }
+
+        public string Name { get; set; }
+
+        public string Cover_Url { get; set; }
+
+        public string Description { get; set; }
+
+        public string Phase { get; set; }
+
+        public string ContestCategory { get; set; }
+        public DateTime EndDate { get; set; }
+
+        //public string ContestType { get; set; }
+
     }
+
 }
