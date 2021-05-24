28d87aecb404d3e6706ee53ab921a2ac373d5d67
boryana.gm <boryana.gm@gmail.com>
2021-05-24 16:58:08 +0300

    Create AwardWinners for Scoring Service

--- src/FullFraim/FullFraim.Services/ScoringServices/ScoringService.cs
+++ src/FullFraim/FullFraim.Services/ScoringServices/ScoringService.cs
@@ -9,6 +9,10 @@ namespace FullFraim.Services.ScoringServices
 {
     public class ScoringService : IScoringService
     {
+        private const string firstWinners = "firstWinners";
+        private const string secondWinners = "secondWinners";
+        private const string thirdWinners = "thirdWinners";
+
         private readonly FullFraimDbContext context;
 
         public ScoringService(FullFraimDbContext context)
@@ -16,8 +20,7 @@ namespace FullFraim.Services.ScoringServices
             this.context = context;
         }
 
-        public async Task AwardWinners(int userId, int contestId) 
-        // Once in Phase III
+        public async Task AwardWinners(int userId, int contestId)                 // Once in Phase III
         {
             var currentContest = await this.context.Contests
                 .Include(c => c.ParticipantContests)
@@ -25,12 +28,12 @@ namespace FullFraim.Services.ScoringServices
 
             var allParticipants = currentContest.ParticipantContests;
 
-            if (allParticipants.Count == 0) // Check if there's no participants in the contest
+            if (allParticipants.Count == 0)                                       // Checks if there are no participants in the contest
             {
                 return;
             }
 
-            var allParticipantsDto = new List<InputScoringDto>();
+            var allParticipantsDto = new List<InputScoringDto>();                 // Lists all participants with their calculated score
 
             foreach (var participant in allParticipants)
             {
@@ -43,77 +46,93 @@ namespace FullFraim.Services.ScoringServices
                 allParticipantsDto.Add(currentParticipant);
             }
 
-            var topThreeScores = allParticipantsDto
+            var topThreeScores = allParticipantsDto                                // Finds top 3 scores and orders by descending (eg. 9.5, 8, 7)
                 .Select(p => p.Score)
                 .ToHashSet()
                 .OrderByDescending(p => p)
                 .Take(3)
                 .ToList();
 
-            var prizeWinnerDict = new Dictionary<string, List<InputScoringDto>>
+            var prizeWinnerDict = new Dictionary<string, List<InputScoringDto>>();  // Holds list of winners for each prize
+
+            prizeWinnerDict[firstWinners] = allParticipantsDto.Where(p => p.Score == topThreeScores[0]).ToList();
+
+            if (topThreeScores.Count > 1)                                           // Checks if there are more than one participants
             {
-                ["firstWinners"] = new List<InputScoringDto>(),
-                ["firstWinners"] = allParticipantsDto.Where(p => p.Score == topThreeScores[0]).ToList(),
+                prizeWinnerDict[secondWinners] = allParticipantsDto.Where(p => p.Score == topThreeScores[1]).ToList();
+            }
 
-                ["secondWinners"] = new List<InputScoringDto>(),
-                ["secondWinners"] = allParticipantsDto.Where(p => p.Score == topThreeScores[1]).ToList(),
+            if (topThreeScores.Count > 2)                                          // Checks if there are more than two participants
+            {
+                prizeWinnerDict[thirdWinners] = allParticipantsDto.Where(p => p.Score == topThreeScores[2]).ToList();
+            }
 
-                ["thirdWinners"] = new List<InputScoringDto>(),
-                ["thirdWinners"] = allParticipantsDto.Where(p => p.Score == topThreeScores[2]).ToList(),
-            };
-
-            CalculateWinnersPoints(prizeWinnerDict, "firstWinners", "secondWinners", "thirdWinners", topThreeScores);
+            UpdateWinnersPoints(prizeWinnerDict, topThreeScores);                  // Updates winners points in database
 
             await this.context.SaveChangesAsync();
         }
 
-        private void CalculateWinnersPoints(Dictionary<string, List<InputScoringDto>> prizeWinnerDict, string first, string second, string third, List<double> topThreeScores)
+        private void UpdateWinnersPoints(Dictionary<string, List<InputScoringDto>> prizeWinnerDict, List<double> topThreeScores)
         {
-            if (prizeWinnerDict[first].Count == 1)
+            foreach (var kvp in prizeWinnerDict)       // Checks how many winners for each prize: first, second and third
             {
-                var firstWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[first][0].UserId);
-                firstWinner.User.Points += 50;
-
-                if (FirstScoreDoublesTheSecond(topThreeScores))
+                if (kvp.Key == firstWinners)
                 {
-                    firstWinner.User.Points += 25;
+                    foreach (var winner in kvp.Value)  // Finds all the first winners and updates their points
+                    {
+                        var firstWinner = this.context.ParticipantContests.FirstOrDefault(pc => pc.UserId == winner.UserId);
+
+                        if (kvp.Value.Count == 1)
+                        {
+                            firstWinner.User.Points += 50;
+
+                            if (FirstScoreDoublesTheSecond(topThreeScores))
+                            {
+                                firstWinner.User.Points += 25;
+                            }
+                        }
+                        else
+                        {
+                            firstWinner.User.Points += 40;
+
+                            if (FirstScoreDoublesTheSecond(topThreeScores))
+                            {
+                                firstWinner.User.Points += 35;
+                            }
+                        }
+                    }
                 }
-            }
-            else
-            {
-                for (int i = 0; i < prizeWinnerDict[first].Count; i++)
+                else if (kvp.Key == secondWinners)
                 {
-                    var firstWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[first][i].UserId);
+                    foreach (var winner in kvp.Value)  // Finds all the second winners and updates their points
+                    {
+                        var secondWinner = this.context.ParticipantContests.FirstOrDefault(pc => pc.UserId == winner.UserId);
 
-                    firstWinner.User.Points += 40;
+                        if (kvp.Value.Count == 1)
+                        {
+                            secondWinner.User.Points += 35;
+                        }
+                        else
+                        {
+                            secondWinner.User.Points += 25;
+                        }
+                    }
                 }
-            }
-
-            if (prizeWinnerDict[second].Count == 1) // Check if empty
-            {
-                var secondWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[second][0].UserId);
-                secondWinner.User.Points += 35;
-            }
-            else
-            {
-                for (int i = 0; i < prizeWinnerDict[second].Count; i++)
+                else
                 {
-                    var secondWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[second][i].UserId);
-                    secondWinner.User.Points += 25;
-                }
-            }
+                    foreach (var winner in kvp.Value)  // Finds all the third winners and updates their points
+                    {
+                        var thirdWinner = this.context.ParticipantContests.FirstOrDefault(pc => pc.UserId == winner.UserId);
 
-            if (prizeWinnerDict[third].Count == 1) // Check if empty
-            {
-                var thirdWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[third][0].UserId);
-                thirdWinner.User.Points += 20;
-            }
-            else
-            {
-                for (int i = 0; i < prizeWinnerDict[third].Count; i++)
-                {
-                    var thirdWinner = this.context.ParticipantContests.First(pc => pc.UserId == prizeWinnerDict[third][i].UserId);
-                    thirdWinner.User.Points += 10;
+                        if (kvp.Value.Count == 1)
+                        {
+                            thirdWinner.User.Points += 20;
+                        }
+                        else
+                        {
+                            thirdWinner.User.Points += 10;
+                        }
+                    }
                 }
             }
         }
@@ -130,7 +149,7 @@ namespace FullFraim.Services.ScoringServices
 
         private double CalculateUserScore(int userId, int contestId)
         {
-            var userReviews = this.context.PhotoReviews
+            var userReviews = this.context.PhotoReviews                      // Finds all the reviews for the current user' photo
                 .Where(pr => pr.Photo.Participant.UserId == userId &&
                 pr.Photo.Participant.ContestId == contestId);
 
@@ -145,7 +164,7 @@ namespace FullFraim.Services.ScoringServices
             }
             else
             {
-                userFinalScore = 3;         // If a photo is not reviewed, a default score of 3 is awarded
+                userFinalScore = 3;                                          // If a photo is not reviewed, a default score of 3 is awarded
             }
 
             return userFinalScore;
