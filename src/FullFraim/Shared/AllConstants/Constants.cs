08594fbfd87741090ad38ad90f92b1b61b75942c
IvanDichev <i.dichev0@gmail.com>
2021-05-26 17:07:15 +0300

    Add trigger to constatns

--- src/FullFraim/Shared/AllConstants/Constants.cs
+++ src/FullFraim/Shared/AllConstants/Constants.cs
@@ -367,5 +367,40 @@ padding:10px 30px 10px 30px!important;
         {
             public const string PasswordResetTokenSuccessfullySend = "Password reset token successfully send";
         }
+
+        public static class DatabaseQueries
+        {
+            public const string RankTrigger = @"CREATE TRIGGER TR_AspNetUsers_UpdateRanks_AU_AI ON dbo.AspNetUsers AFTER INSERT,UPDATE AS
+BEGIN
+		DECLARE TrigTempUpdate_Cursor CURSOR FOR
+			SELECT Id FROM Inserted
+
+		Declare @Id int;
+
+		OPEN TrigTempUpdate_Cursor;
+		FETCH NEXT FROM TrigTempUpdate_Cursor INTO @Id
+
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+		UPDATE AspNetUsers SET RankId = (
+			CASE 
+				WHEN (SELECT Points FROM AspNetUsers WHERE Id = @Id) > 1000
+					THEN (SELECT Id FROM Ranks WHERE [Name] = 'Wise and Benevolent Photo Dictator')
+				WHEN (SELECT Points FROM AspNetUsers WHERE Id = @Id) > 150
+					THEN (SELECT Id FROM Ranks WHERE [Name] = 'Master')
+				WHEN (SELECT Points FROM AspNetUsers WHERE Id = @Id) > 50
+					THEN (SELECT Id FROM Ranks WHERE [Name] = 'Enthusiast')
+				ELSE (SELECT Id FROM Ranks WHERE [Name] = 'Junkie')
+			END)
+			WHERE id = @Id
+
+		FETCH NEXT FROM TrigTempUpdate_Cursor INTO @Id
+		END;
+
+		CLOSE TrigTempUpdate_Cursor;
+		DEALLOCATE TrigTempUpdate_Cursor;  
+END;";
+            public const string DropRankTrigger = @"DROP TRIGGER TR_AspNetUsers_UpdateRanks_AU_AI";
+        }
     }
 }
