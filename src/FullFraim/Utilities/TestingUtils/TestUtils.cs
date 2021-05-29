02d544054562dd4b9658c675c9b5f7677043bd53
boryana.gm <boryana.gm@gmail.com>
2021-05-29 20:55:07 +0300

    Seed ContestPhase in TestUtils

--- src/FullFraim/Utilities/TestingUtils/TestUtils.cs
+++ src/FullFraim/Utilities/TestingUtils/TestUtils.cs
@@ -1,6 +1,7 @@
 ﻿using FullFraim.Data.Models;
 using Microsoft.EntityFrameworkCore;
 using Shared;
+using System;
 using System.Collections.Generic;
 
 namespace Utilities.TestingUtils
@@ -72,6 +73,99 @@ namespace Utilities.TestingUtils
             };
         }
 
+        public static ICollection<ContestPhase> GetContestPhases()
+        {
+            return new List<ContestPhase>()
+            {
+                // Contest PhaseOne
+                new ContestPhase()
+                {
+                    ContestId = 1,
+                    PhaseId = 1,
+                    StartDate = DateTime.UtcNow,
+                    EndDate = DateTime.UtcNow.AddDays(30),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 1,
+                    PhaseId = 2,
+                    StartDate = DateTime.UtcNow.AddDays(30),
+                    EndDate = DateTime.UtcNow.AddDays(60),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 1,
+                    PhaseId = 3,
+                    StartDate = DateTime.UtcNow.AddDays(90),
+                    EndDate = DateTime.MaxValue,
+                },
+                // Contest PhaseTwo
+                new ContestPhase()
+                {
+                    ContestId = 2,
+                    PhaseId = 1,
+                    StartDate = DateTime.UtcNow.AddDays(-30),
+                    EndDate = DateTime.UtcNow,
+                },
+                new ContestPhase()
+                {
+                    ContestId = 2,
+                    PhaseId = 2,
+                    StartDate = DateTime.UtcNow,
+                    EndDate = DateTime.UtcNow.AddDays(30),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 2,
+                    PhaseId = 3,
+                    StartDate = DateTime.UtcNow.AddDays(30),
+                    EndDate = DateTime.MaxValue,
+                },
+                // Contest Finished
+                new ContestPhase()
+                {
+                    ContestId = 3,
+                    PhaseId = 1,
+                    StartDate = DateTime.UtcNow.AddDays(-2),
+                    EndDate = DateTime.UtcNow.AddDays(-1),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 3,
+                    PhaseId = 2,
+                    StartDate = DateTime.UtcNow.AddDays(-1),
+                    EndDate = DateTime.UtcNow,
+                },
+                new ContestPhase()
+                {
+                    ContestId = 3,
+                    PhaseId = 3,
+                    StartDate = DateTime.UtcNow,
+                    EndDate = DateTime.MaxValue,
+                },
+                new ContestPhase()
+                {
+                    ContestId = 4,
+                    PhaseId = 1,
+                    StartDate = DateTime.UtcNow,
+                    EndDate = DateTime.UtcNow.AddDays(30),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 4,
+                    PhaseId = 2,
+                    StartDate = DateTime.UtcNow.AddDays(30),
+                    EndDate = DateTime.UtcNow.AddDays(60),
+                },
+                new ContestPhase()
+                {
+                    ContestId = 4,
+                    PhaseId = 3,
+                    StartDate = DateTime.UtcNow.AddDays(60),
+                    EndDate = DateTime.MaxValue,
+                },
+            };
+        }
         public static ICollection<Photo> GetPhotos()
         {
             return new List<Photo>()
@@ -171,6 +265,6 @@ namespace Utilities.TestingUtils
             };
         }
 
-       
+
     }
 }
