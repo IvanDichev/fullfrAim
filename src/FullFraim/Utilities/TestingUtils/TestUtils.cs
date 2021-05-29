07f318d573c2f5c30757fc8870c0066705ee4382
boryana.gm <boryana.gm@gmail.com>
2021-05-29 21:04:02 +0300

    Seed ParticipantContest in TestUtils

--- src/FullFraim/Utilities/TestingUtils/TestUtils.cs
+++ src/FullFraim/Utilities/TestingUtils/TestUtils.cs
@@ -247,6 +247,94 @@ namespace Utilities.TestingUtils
                 },
             };
         }
+        public static ICollection<ParticipantContest> GetParticipantContests()
+        {
+            return new List<ParticipantContest>()
+            {
+                 // PhaseOne contest
+                new ParticipantContest()
+                {
+                    ContestId = 1,
+                    UserId = 2,
+                    PhotoId = 1,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 1,
+                    UserId = 3,
+                    PhotoId = 2,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 1,
+                    UserId = 4,
+                    PhotoId = 3,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 1,
+                    UserId = 5,
+                    PhotoId = 4,
+                },
+                // PhaseTwo contest
+                new ParticipantContest()
+                {
+                    ContestId = 2,
+                    UserId = 2,
+                    PhotoId = 5,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 2,
+                    UserId = 3,
+                    PhotoId = 6,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 2,
+                    UserId = 4,
+                    PhotoId = 7,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 2,
+                    UserId = 5,
+                    PhotoId = 8,
+                },
+                // Finished contest
+                new ParticipantContest()
+                {
+                    ContestId = 3,
+                    UserId = 2,
+                    PhotoId = 9,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 3,
+                    UserId = 3,
+                    PhotoId = 10,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 3,
+                    UserId = 4,
+                    PhotoId = 11,
+                },
+                new ParticipantContest()
+                {
+                    ContestId = 3,
+                    UserId = 5,
+                    PhotoId = 12,
+                },
+                // Portraits contest
+                new ParticipantContest()
+                {
+                    ContestId = 4,
+                    UserId = 6,
+                    PhotoId = 13,
+                },
+            };
+        }
         public static ICollection<Photo> GetPhotos()
         {
             return new List<Photo>()
