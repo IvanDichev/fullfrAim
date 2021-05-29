87941346b409023967f25b1f94f09ca89a512c97
Valentin Shikov <valentin.shikov.a26@learn.telerikacademy.com>
2021-05-29 18:53:05 +0300

    Implement base classes for Testing

---
+++ src/FullFraim/FullFraim.Tests/JuryService/JuryServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FullFraim.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.TestingUtils;
+
+namespace FullFraim.Tests.JuryService
+{
+    [TestClass]
+    public class JuryServiceTests
+    {
+        [TestMethod]
+        public async Task GetAll_ShouldReturnAllJuries()
+        {
+            //Arrange
+            var options = TestUtils
+                .GetInMemoryDatabaseOptions<FullFraimDbContext>(nameof(GetAll_ShouldReturnAllJuries));
+
+            using (var dbContext = new DeliverITContext(options))
+            {
+                dbContext.Countries.AddRange(Util.GetCountries());
+                dbContext.Cities.AddRange(Util.GetCities());
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            using (var context = new DeliverITContext(options))
+            {
+                var cityService = new CityService(context);
+
+                var param = new PaginationParams();
+
+                //Act
+                var cities = await cityService.GetAllAsync(param);
+
+                //Assert
+                Assert.AreEqual(Util.GetCities().Count, cities.Count);
+
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
---
+++ src/FullFraim/Utilities/TestingUtils/TestUtils.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Utilities.TestingUtils
+{
+    public class TestUtils
+    {
+        public static DbContextOptions<T> GetInMemoryDatabaseOptions<T>(string dbName)
+            where T : DbContext
+        {
+            var options = new DbContextOptionsBuilder<T>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+
+            return options;
+        }
+    }
+}
