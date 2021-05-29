e963d846f4def1487e911219dd3784016afb0f3c
boryana.gm <boryana.gm@gmail.com>
2021-05-29 20:47:53 +0300

    Seed Photos in TestUtils

---
+++ src/FullFraim/FullFraim.Tests/ScoringService/ScoringServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using FullFraim.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Utilities.TestingUtils;
+
+namespace FullFraim.Tests.ScoringService
+{
+    [TestClass]
+    public class ScoringServiceTests
+    {
+        [TestMethod]
+        public async Task CalculateUserScore_Should_CorrectlyCalculateScore()
+        {
+            //Arrange
+            var options = TestUtils
+                .GetInMemoryDatabaseOptions<FullFraimDbContext>(nameof(CalculateUserScore_Should_CorrectlyCalculateScore));
+
+            using (var dbContext = new FullFraimDbContext(options))
+            {
+                
+            }
+
+
+        }
+    }
+}
---
+++ src/FullFraim/Utilities/TestingUtils/TestUtils.cs
@@ -0,0 +1,119 @@
+﻿using FullFraim.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Shared;
+using System.Collections.Generic;
+
+namespace Utilities.TestingUtils
+{
+    public static class TestUtils
+    {
+        public static DbContextOptions<T> GetInMemoryDatabaseOptions<T>(string dbName)
+            where T : DbContext
+        {
+            var options = new DbContextOptionsBuilder<T>()
+                .UseInMemoryDatabase(dbName)
+                .Options;
+
+            return options;
+        }
+
+        public static ICollection<Photo> GetPhotos()
+        {
+            return new List<Photo>()
+            {
+                new Photo()
+                {
+                    ContestId = 1,
+                    Title = "Squirrel",
+                    Story = "Looking down",
+                    Url = Constants.ImagesSeed.WildlifeImgUrl,
+                },
+                new Photo()
+                {
+                    ContestId = 1,
+                    Title = "Bath time",
+                    Story = "On my way",
+                    Url = Constants.ImagesSeed.WildlifeImg2Url,
+                },
+                new Photo()
+                {
+                    ContestId = 1,
+                    Title = "Fight in the night",
+                    Story = "Subway fighters",
+                    Url = Constants.ImagesSeed.WildlifeImg3Url,
+                },
+                new Photo()
+                {
+                    ContestId = 1,
+                    Title = "I can climb it",
+                    Story = "Not a long way, we can climb it",
+                    Url = Constants.ImagesSeed.WildlifeImg4Url,
+                },
+                new Photo()
+                {
+                    ContestId = 2,
+                    Title = "Fight in the night",
+                    Story = "Subway fighters",
+                    Url = Constants.ImagesSeed.WildlifeImg3Url,
+                },
+                new Photo()
+                {
+                    ContestId = 2,
+                    Title = "I can climb it",
+                    Story = "Not a long way, we can climb it",
+                    Url = Constants.ImagesSeed.WildlifeImg4Url,
+                },
+                new Photo()
+                {
+                    ContestId = 2,
+                    Title = "Can I have some?",
+                    Story = "Hungry birds",
+                    Url = Constants.ImagesSeed.WildlifeImg5Url,
+                },
+                new Photo()
+                {
+                    ContestId = 2,
+                    Title = "Git It!",
+                    Story = "Got it!",
+                    Url = Constants.ImagesSeed.WildlifeImg6Url,
+                },
+
+                new Photo()
+                {
+                    ContestId = 3,
+                    Title = "Squirrel",
+                    Story = "Looking down",
+                    Url = Constants.ImagesSeed.WildlifeImgUrl,
+                },
+                new Photo()
+                {
+                    ContestId = 3,
+                    Title = "Bath time",
+                    Story = "On my way",
+                    Url = Constants.ImagesSeed.WildlifeImg2Url,
+                },
+                new Photo()
+                {
+                    ContestId = 3,
+                    Title = "Fight in the night",
+                    Story = "Subway fighters",
+                    Url = Constants.ImagesSeed.WildlifeImg3Url,
+                },
+                new Photo()
+                {
+                    ContestId = 3,
+                    Title = "I can climb it",
+                    Story = "Not a long way, we can climb it",
+                    Url = Constants.ImagesSeed.WildlifeImg4Url,
+                },
+                new Photo()
+                {
+                    ContestId = 4,
+                    Title = "Smile",
+                    Story = "Just a nice picture",
+                    Url = Constants.ImagesSeed.PortraitImgUrlCover,
+                }
+            };
+        }
+    }
+}
