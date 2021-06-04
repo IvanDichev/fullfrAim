a07733af7ced5466318c3e0ae416f1c6f4c75f72
Valentin Shikov <valentin.shikov.a26@learn.telerikacademy.com>
2021-06-05 01:24:16 +0300

    Implement Current User in ViewComponent

--- src/FullFraim/FullFraim.Web/ViewComponents/PointsTillNextViewComponent.cs
+++ src/FullFraim/FullFraim.Web/ViewComponents/PointsTillNextViewComponent.cs
@@ -1,4 +1,5 @@
-﻿using FullFraim.Data.Models;
+﻿using FullFraim.Data;
+using FullFraim.Data.Models;
 using FullFraim.Models.ViewModels.Dashboard;
 using FullFraim.Services.PhotoJunkieServices;
 using Microsoft.AspNetCore.Identity;
@@ -18,14 +19,18 @@ namespace FullFraim.Web.ViewComponents
         private readonly IPhotoJunkieService photoJunkieService;
         private readonly UserManager<User> userManager;
         private readonly IConfiguration configuration;
+        private readonly FullFraimDbContext context;
 
         public PointsTillNextViewComponent
             (IPhotoJunkieService photoJunkieService,
-            UserManager<User> userManager, IConfiguration configuration)
+            UserManager<User> userManager, 
+            IConfiguration configuration,
+            FullFraimDbContext context)
         {
             this.photoJunkieService = photoJunkieService;
             this.userManager = userManager;
             this.configuration = configuration;
+            this.context = context;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
@@ -51,10 +56,38 @@ namespace FullFraim.Web.ViewComponents
                 result
                     .Add(photojunkieWithPoints
                     .MapToPointsViewModel
-                    (junkie.FirstName + junkie.LastName));
+                    ($"{junkie.FirstName} {junkie.LastName}"));
             }
 
+            result = result
+                .OrderBy(r => r.PointsTillNextRank)
+                .ToList();
+
+            ViewBag.CurrentUser = await GetCurrentUserPointsAsync();
+
             return View(result);
+        }
+
+        private async Task<CurrentUserViewModel> GetCurrentUserPointsAsync()
+        {
+            var id = int.Parse(HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+
+            var user = await this.context.Users
+                .Where(x => x.Id == id)
+                .Select(x => new { x.FirstName, x.LastName, x.Points})
+                .FirstOrDefaultAsync();
+
+            var pointsTillNext = await photoJunkieService
+                    .GetPointsTillNextRankAsync(id);
+
+            var result = new CurrentUserViewModel()
+            {
+                CurrentPoints = (int)user.Points,
+                Ranking = pointsTillNext.MapToPointsViewModel($"{user.FirstName} {user.LastName}"),
+                Rank = pointsTillNext.Rank,
+            };
+
+            return result;
         }
     }
 }
